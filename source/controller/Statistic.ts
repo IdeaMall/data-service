a7374fc45ecd6280e7afdb2654eceec34be65d56
TechQuery <shiy2008@gmail.com>
2023-02-14 16:19:47 +0800

    [add] Statistic controller [fix] 2 detail bugs

---
+++ source/controller/Statistic.ts
@@ -0,0 +1,37 @@
+import { StatisticSummary } from '@ideamall/data-model';
+import { Get, JsonController } from 'routing-controllers';
+import { ResponseSchema } from 'routing-controllers-openapi';
+
+import dataSource, { Category, Goods, Order, Parcel, User } from '../model';
+
+@JsonController('/statistic')
+export class StatisticController {
+    @Get()
+    @ResponseSchema(StatisticSummary)
+    async getStatisticSummary(): Promise<StatisticSummary> {
+        const [
+            userCount,
+            categoryCount,
+            goodsCount,
+            orderCount,
+            parcelCount,
+            commentCount
+        ] = await Promise.all([
+            dataSource.getRepository(User).count(),
+            dataSource.getRepository(Category).count(),
+            dataSource.getRepository(Goods).count(),
+            dataSource.getRepository(Order).count(),
+            dataSource.getRepository(Parcel).count(),
+            dataSource.getRepository(Comment).count()
+        ]);
+
+        return {
+            userCount,
+            categoryCount,
+            goodsCount,
+            orderCount,
+            parcelCount,
+            commentCount
+        };
+    }
+}
--- source/controller/index.ts
+++ source/controller/index.ts
@@ -7,19 +7,22 @@ import { CommentController } from './Comment';
 import { FavoriteController } from './Favorite';
 import { GoodsController } from './Goods';
 import { OrderController } from './Order';
+import { StatisticController } from './Statistic';
 import { UserController } from './User';
 
+export * from './Statistic';
 export * from './User';
 
 export const { swagger, mocker, router } = createAPI({
     mock: !isProduct,
     controllers: [
-        UserController
+        UserController,
         // AddressController,
         // CategoryController,
         // GoodsController,
         // FavoriteController,
         // OrderController,
         // CommentController
+        StatisticController
     ]
 });
